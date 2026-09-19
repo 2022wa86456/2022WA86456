4bfe82f2cc2249dbeabe82dda1eb22f4a36ff74c
2022wa86456 <2022wa86456@wilp.bits-pilani.ac.in>
2026-09-19 18:31:14 +0530

    Lab assignment - part4

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Devops Lab Assignment - 2022WA86456 );
     }
 }
